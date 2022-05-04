d2b3c125e68ac822d71799f26a5757c84005a10c
Sangwoo <bsu7959@gmail.com>
2022-05-04 18:49:36 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
